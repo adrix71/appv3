91c951a5b3be6a9ad943b707cabc7562a9ad4614
adrix71 <agisoftt@gmail.com>
2018-10-06 14:14:19 +0200

    Update MySQL schema

--- app/database/app.mysql.sql
+++ app/database/app.mysql.sql
@@ -1,3 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Creato il: Ott 06, 2018 alle 12:10
+-- Versione del server: 5.7.21
+-- Versione PHP: 7.1.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appv3`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -6,17 +30,17 @@
 
 DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
-`id` int(16) NOT NULL AUTO_INCREMENT,
-`parent_id` int(16) DEFAULT NULL,
-`name` varchar(255) NOT NULL,
-`verification` int(1) NOT NULL,
-`approval` int(1) NOT NULL,
-`email_as_username` int(1) NOT NULL,
-`email_required` int(1) NOT NULL,
-`permissions` text,
-PRIMARY KEY (`id`),
-KEY `role_name` (`name`),
-KEY `fk_role_parent_id` (`parent_id`)
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(16) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `verification` int(1) NOT NULL,
+  `approval` int(1) NOT NULL,
+  `email_as_username` int(1) NOT NULL,
+  `email_required` int(1) NOT NULL,
+  `permissions` text,
+  PRIMARY KEY (`id`),
+  KEY `role_name` (`name`),
+  KEY `fk_role_parent_id` (`parent_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2010 DEFAULT CHARSET=utf8;
 
 --
@@ -35,21 +59,21 @@ INSERT INTO `roles` (`id`, `parent_id`, `name`, `verification`, `approval`, `ema
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(16) NOT NULL AUTO_INCREMENT,
-`role_id` int(16) DEFAULT NULL,
-`username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-`password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-`email` varchar(255) DEFAULT NULL,
-`active` int(1) DEFAULT NULL,
-`verified` int(1) DEFAULT NULL,
-`last_login` datetime DEFAULT NULL,
-`last_ip` varchar(255) DEFAULT NULL,
-`last_ua` varchar(255) DEFAULT NULL,
-`total_logins` int(16) DEFAULT '0',
-`failed_attempts` int(16) DEFAULT '0',
-PRIMARY KEY (`id`),
-KEY `role_id` (`role_id`),
-KEY `username` (`username`)
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `role_id` int(16) DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `active` int(1) DEFAULT NULL,
+  `verified` int(1) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `last_ip` varchar(255) DEFAULT NULL,
+  `last_ua` varchar(255) DEFAULT NULL,
+  `total_logins` int(16) DEFAULT '0',
+  `failed_attempts` int(16) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8;
 
 --
@@ -57,7 +81,7 @@ KEY `username` (`username`)
 --
 
 INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `email`, `active`, `verified`, `last_login`, `last_ip`, `last_ua`, `total_logins`, `failed_attempts`) VALUES
-(1001, 2001, 'admin', '$2y$08$ckh6UXNYdjdSVzhlcWh2OOCrjBWHarr8Fxf3i2BYVlC29Ag/eoGkC', '', 1, 1, '2018-09-29 21:05:11', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0', 68, 0);
+(1001, 2001, 'admin', '$2y$08$ckh6UXNYdjdSVzhlcWh2OOCrjBWHarr8Fxf3i2BYVlC29Ag/eoGkC', '', 1, 1, '2018-09-29 21:05:11', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0', 70, 0);
 
 -- --------------------------------------------------------
 
@@ -67,14 +91,22 @@ INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `email`, `active`,
 
 DROP TABLE IF EXISTS `user_logins`;
 CREATE TABLE IF NOT EXISTS `user_logins` (
-`id` int(16) NOT NULL AUTO_INCREMENT,
-`user_id` int(16) DEFAULT NULL,
-`ip` varchar(255) NOT NULL,
-`ua` varchar(255) NOT NULL,
-`timestamp` datetime NOT NULL,
-PRIMARY KEY (`id`),
-KEY `fk_user_login_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `user_id` int(16) DEFAULT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ua` varchar(255) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_login_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `user_logins`
+--
+
+INSERT INTO `user_logins` (`id`, `user_id`, `ip`, `ua`, `timestamp`) VALUES
+(1, 1001, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0', '2018-10-05 14:57:31'),
+(2, 1001, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0', '2018-10-05 21:30:59');
 
 -- --------------------------------------------------------
 
@@ -84,17 +116,21 @@ KEY `fk_user_login_id` (`user_id`)
 
 DROP TABLE IF EXISTS `user_sessions`;
 CREATE TABLE IF NOT EXISTS `user_sessions` (
-`id` int(16) NOT NULL AUTO_INCREMENT,
-`user_id` int(16) DEFAULT NULL,
-`session_id` varchar(255) NOT NULL,
-`ip` varchar(255) NOT NULL,
-`ua` varchar(255) NOT NULL,
-`start` int(16) NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `session_key` (`id`,`user_id`,`session_id`),
-KEY `fk_user_session_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(16) NOT NULL AUTO_INCREMENT,
+  `user_id` int(16) DEFAULT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `ua` varchar(255) NOT NULL,
+  `start` int(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `session_key` (`id`,`user_id`,`session_id`),
+  KEY `fk_user_session_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+--
+-- Dump dei dati per la tabella `user_sessions`
+--
+\
 --
 -- Limiti per le tabelle scaricate
 --
@@ -103,23 +139,27 @@ KEY `fk_user_session_id` (`user_id`)
 -- Limiti per la tabella `roles`
 --
 ALTER TABLE `roles`
-ADD CONSTRAINT `fk_role_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_role_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `users`
 --
 ALTER TABLE `users`
-ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `user_logins`
 --
 ALTER TABLE `user_logins`
-ADD CONSTRAINT `fk_user_login_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_user_login_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `user_sessions`
 --
 ALTER TABLE `user_sessions`
-ADD CONSTRAINT `fk_user_session_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_user_session_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
